74dfdbf2b6e02e499829257b69db94ffbcc3e8dd
Liis Lensment <liis.lensment@gmail.com>
2024-04-25 22:40:16 +0300

    add week7 code

---
+++ Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int secondNum = Int32.Parse(Console.ReadLine());
+
+switch (userOperator)
+{
+    case '+':
+        addition(firstNum, secondNum);
+        break;
+    case '-':
+        subtraction(firstNum, secondNum);
+        break;
+    default:
+        Console.WriteLine("invalid operator");
+        break;
+}
+
+static void addition(int a, int b)
+{
+    Console.WriteLine($"{a} + {b} = {a + b}");
+}
+
+static void subtraction(int a, int b)
+{
+    Console.WriteLine($"{a} - {b} = {a - b}");
+}
+
---
+++ Week7Functions/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userchoice = Console.ReadLine();
+
+if (userchoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+
+static void PrintHello() //funtion / method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
+
+
